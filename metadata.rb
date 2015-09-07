cdba451197087fafeb9546f11ba1b41659a5920c
Tim Smith <tsmith@chef.io>
2015-09-07 13:21:45 -0700

    Update maintainer and add fedora as a supported platform

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name 'yum-fedora'
-maintainer 'Chef'
-maintainer_email 'Sean OMeara <someara@chef.io>'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-fedora'
 version '0.3.0'
 
 depends 'yum', '~> 3.0'
+supports 'fedora'
