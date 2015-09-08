ea668b9666df34b48764ef9c39dd1c64d3cb43f6
Tim Smith <tsmith84@gmail.com>
2015-09-07 22:06:37 -0700

    Add long_description and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,11 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-fedora'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.3.0'
 
 depends 'yum', '~> 3.0'
 supports 'fedora'
+
+source_url 'https://github.com/chef-cookbooks/yum-fedora' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-fedora/issues' if respond_to?(:issues_url)
