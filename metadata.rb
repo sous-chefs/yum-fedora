d360e0a239404a8a3c4d87e9488c9fb63b2df6f2
Tim Smith <tsmith84@gmail.com>
2015-09-21 17:24:18 -0700

    Better description and require Yum 3.2

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,11 @@ name 'yum-fedora'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Installs/Configures yum-fedora'
+description 'Installs and configures the Fedora Yum repositories'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.3.0'
 
-depends 'yum', '~> 3.0'
+depends 'yum', '~> 3.2'
 supports 'fedora'
 
 source_url 'https://github.com/chef-cookbooks/yum-fedora' if respond_to?(:source_url)
