7486eea397bda913d7457b413fab42761d858760
Tim Smith <tsmith@chef.io>
2016-09-01 11:05:53 -0700

    Add chef_version metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures the Fedora Yum repositories'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.3.4'
 
 depends 'yum', '~> 3.2'
@@ -10,3 +11,5 @@ supports 'fedora'
 
 source_url 'https://github.com/chef-cookbooks/yum-fedora' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/yum-fedora/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11' if respond_to?(:chef_version)
