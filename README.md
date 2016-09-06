bd49f407c5fe2c013e069d4b94620f095be03612
Tim Smith <tsmith@chef.io>
2016-09-06 14:57:04 -0700

    Remove support for Chef 11
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ The yum-fedora cookbook takes over management of the default repositoryids that 
 - Fedora
 
 ### Chef
-- Chef 11+
+- Chef 12+
 
 ### Cookbooks
 - yum version 3.2.0 or higher
--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,10 @@ description 'Installs and configures the Fedora Yum repositories'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.3.4'
 
-depends 'yum', '~> 3.2'
+depends 'yum', '>= 3.2', '< 5.0'
 supports 'fedora'
 
-source_url 'https://github.com/chef-cookbooks/yum-fedora' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/yum-fedora/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/yum-fedora'
+issues_url 'https://github.com/chef-cookbooks/yum-fedora/issues'
 
-chef_version '>= 11' if respond_to?(:chef_version)
+chef_version '>= 12'
