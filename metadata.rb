0289f9359d0b6a8288577911e80868691b2d4cab
Tim Smith <tsmith@chef.io>
2016-10-03 23:16:23 -0700

    Replace yum dependency with compat_resource

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,10 @@ description 'Installs and configures the Fedora Yum repositories'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
-depends 'yum', '>= 3.2', '< 5.0'
+depends 'compat_resource', '>= 12.14.1'
 supports 'fedora'
 
 source_url 'https://github.com/chef-cookbooks/yum-fedora'
 issues_url 'https://github.com/chef-cookbooks/yum-fedora/issues'
 
-chef_version '>= 12'
+chef_version '>= 12.1'
