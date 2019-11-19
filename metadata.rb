b536fb956db3d02ec18392e9a8a44ecfa1296a6a
Tim Smith <tsmith84@gmail.com>
2019-11-18 16:21:13 -0800

    Cleanup cookstyle warnings in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,10 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures the Fedora Yum repositories'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '3.0.0'
 
 supports 'fedora'
 
 source_url 'https://github.com/chef-cookbooks/yum-fedora'
 issues_url 'https://github.com/chef-cookbooks/yum-fedora/issues'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 12.14'
