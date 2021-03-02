b1372a2a94934caf31d6c209f604747a840709b7
Ryan <kennedy@osuosl.org>
2021-03-02 14:58:28 -0800

    remove cookboook

--- Berksfile
+++ Berksfile
@@ -1,5 +1,3 @@
 source 'https://supermarket.chef.io'
 
-cookbook 'fedora-test', path: 'test/cookbooks/fedora-test'
-
 metadata
--- test/cookbooks/fedora-test/metadata.rb
+++
@@ -1,9 +0,0 @@
-name              'fedora-test'
-license           'Apache-2.0'
-description       'Installs and configures the Fedora Yum repositories'
-version           '3.0.0'
-chef_version      '>= 12.15'
-
-supports 'fedora'
-
-depends  'yum-fedora'
